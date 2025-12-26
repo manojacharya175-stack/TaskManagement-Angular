108c94fa1982c0128d0a85c502779e998d7227b5
Manoj Kumar MS <250993613+manojacharya175-stack@users.noreply.github.com>
2025-12-26 10:08:41 +0530

    Added auth handling in interceptor

--- src/app/core/auth/jwt.interceptor.ts
+++ src/app/core/auth/jwt.interceptor.ts
@@ -1,16 +1,29 @@
 import { HttpInterceptorFn } from "@angular/common/http"; 
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError } from "rxjs";
+import { throwError } from "rxjs";
+
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+        const router = inject(Router);
         const token =  localStorage.getItem('auth_token');
 
-        if(token){
-            const authReq = req.clone({
-                setHeaders:{
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            return next(authReq);
-        }
+        const authReq = token 
+        ? req.clone({
+            setHeaders: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        : req;
 
-    return next(req);
-}
+        return next(authReq).pipe(
+            catchError(err => {
+                if(err.status === 401 || err.status === 403){
+                    localStorage.removeItem('auth_token');
+                    router.navigate(['/login']);
+                }
+                return throwError(() => err);
+            })
+        );
+};
