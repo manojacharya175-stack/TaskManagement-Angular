e2785fcc07bd3bc126dcf5eea868cff3b4962546
Manoj Kumar MS <250993613+manojacharya175-stack@users.noreply.github.com>
2025-12-26 10:45:39 +0530

    UX improvements hide header and clean layout

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,17 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../../core/auth/auth';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.html',
-  styleUrls: ['./header.css']
+  styleUrls: ['./header.css'],
+  imports: [CommonModule]
 })
 export class Header {
 
   private authService = inject(Auth);
   private router = inject(Router);
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 
   logout() {
     this.authService.logout();
